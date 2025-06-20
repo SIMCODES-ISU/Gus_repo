c411e2e52f5d51919c7854ac9f31651ad56ebb00
gus <dwoodard@iastate.edu>
2025-06-20 10:53:21 -0500

    energy reader

--- h2/read_h2_output.py
+++ h2/read_h2_output.py
@@ -4,19 +4,17 @@ from pathlib import Path
 
 def h2_file_path():
     project_root = Path(__file__).resolve().parent.parent
-    return project_root / "output" / "h2_output.txt"
+    return project_root / "h2" / "h2_output.txt"
 # path of file
 
 
 def read(file):
     with open(file, 'r') as f:
         lines = f.readlines()
-# Read all lines into a list
         for i, line in enumerate(lines):
-            if line.split().startswith("Total SCF energy"):
-                print(line)
+            if "Total SCF energy" in line:
+                words = line.split()
+                print("SCF energy =", words[4])
 
 
 read(h2_file_path())
-
-# /Users/guswoodard/Desktop/simcodes/Gus_h2_reader-1/h2/h2_output.txt
---
+++ test/fdsafds.py
@@ -0,0 +1,3 @@
+line = "         Total SCF energy =     -1.100565135890"
+words = line.split()
+print("Energy =", words[4])
