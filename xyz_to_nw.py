5c30ecc2e3e050d4764b1664a3cc50367db66135
gus <dwoodard@iastate.edu>
2025-07-03 14:29:38 -0500

    now change

--- xyz_to_nw.py
+++ xyz_to_nw.py
@@ -4,14 +4,15 @@ from pathlib import Path
 def get_xyz_files():
     directory = Path(__file__).resolve().parent
     return list(directory.rglob("*.xyz"))
-#get all .xyz files in the current directory and subdirectories
+# get all .xyz files in the current directory and subdirectories
+
 
 def xyz_to_nw(xyz_path, output_dir):
     output_file = output_dir / (xyz_path.stem + ".nw")
 # convert .xyz file to .nw file
     with open(xyz_path, 'r') as g:
         lines = g.readlines()
-#opne .xyz file and read lines
+# opne .xyz file and read lines
     with open(output_file, 'w') as f:
         f.write(f"start {xyz_path.stem}\n")
         f.write("geometry\n")
@@ -26,20 +27,20 @@ def xyz_to_nw(xyz_path, output_dir):
         f.write("end\n\n")
 
         f.write("echo\n\n")
-        
+
         f.write("scf\n")
         f.write(" direct\n")
         f.write("end\n\n")
-        
+
         f.write("task scf energy\n")
-#write basis set and SCF task to .nw file
+# write basis set and SCF task to .nw file
+
 
 def batch_write_files_to_desktop():
-  
-    desktop = Path.home() / "Desktop"
+
     output_dir = Path("outputs/nwchem_inputs")
     output_dir.mkdir(exist_ok=True)
-#output directory for .nw files
+# output directory for .nw files
     xyz_files = get_xyz_files()
     for xyz_file in xyz_files:
         xyz_to_nw(xyz_file, output_dir)
